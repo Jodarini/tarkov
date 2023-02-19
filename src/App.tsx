499e6b1c9726ae147d96cab0aa5ef5d2e640ecb9
Jodarini <rivasjose9428@gmail.com>
2023-02-19 17:01:13 -0500

    Button styling for all items pagination

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {
 	QueryClientProvider,
 	useQuery,
 } from "react-query";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const apiEndPoint = "https://api.tarkov.dev/graphql";
 
@@ -16,7 +16,8 @@ export default function App() {
 			<div className="flex flex-col items-center ">
 				<h1 className="text- text-4xl ">Tarkov API</h1>
 				<MenuBar />
-				<TarkovList />
+				<TarkovItems />
+				<TarkovTraders />
 			</div>
 		</QueryClientProvider>
 	);
@@ -35,9 +36,66 @@ function MenuBar() {
 	);
 }
 
-function TarkovList() {
-	const [tarkovItems, setTarkovItems] = useState([]);
+function TarkovTraders() {
 	const [tarkovTraders, setTarkovTraders] = useState([]);
+
+	const allTraders = `query {
+		traders {
+			name,
+			description,
+			imageLink
+		}
+	}`;
+
+	const { data: traderData, status: traderStatus } = useQuery(
+		[allTraders],
+		() => {
+			fetch(apiEndPoint, {
+				method: "post",
+				headers: {
+					"Content-Type": "applicaton/json",
+				},
+				body: JSON.stringify({
+					query: allTraders,
+				}),
+			})
+				.then(res => res.json())
+				.then(res => {
+					setTarkovTraders(res.data.traders);
+				});
+		}
+	);
+
+	return (
+		<div>
+			All traders
+			<table className="table-auto border-gray-200 border">
+				<tbody>
+					{tarkovTraders.map(
+						(trader: {
+							name: string;
+							description: string;
+							imageLink: string;
+						}) => (
+							<tr className="border " key={trader.name}>
+								<th className="border">
+									<img src={trader.imageLink} alt="trader image" />
+									{trader.name}
+								</th>
+								<th className="border p-4 text-left font-light">
+									{trader.description}
+								</th>
+							</tr>
+						)
+					)}
+				</tbody>
+			</table>
+		</div>
+	);
+}
+
+function TarkovItems() {
+	const [tarkovItems, setTarkovItems] = useState([]);
 	const [page, setPage] = useState(0);
 
 	const itemsPerPage = 10;
@@ -49,14 +107,6 @@ function TarkovList() {
       shortName
     }
   	}`;
-
-	const allTraders = `query {
-		traders {
-			name,
-			description,
-			imageLink
-		}
-	}`;
 
 	const { data, status } = useQuery(
 		[allItems, page, itemsPerPage],
@@ -81,25 +131,6 @@ function TarkovList() {
 		}
 	);
 
-	const { data: traderData, status: traderStatus } = useQuery(
-		[allTraders],
-		() => {
-			fetch(apiEndPoint, {
-				method: "post",
-				headers: {
-					"Content-Type": "applicaton/json",
-				},
-				body: JSON.stringify({
-					query: allTraders,
-				}),
-			})
-				.then(res => res.json())
-				.then(res => {
-					setTarkovTraders(res.data.traders);
-				});
-		}
-	);
-
 	const nextPage = () => {
 		setPage(prev => prev + 10);
 	};
@@ -110,30 +141,6 @@ function TarkovList() {
 
 	return (
 		<div>
-			<div>
-				All traders
-				<table className="table-auto border-gray-200 border">
-					<tbody>
-						{tarkovTraders.map(
-							(trader: {
-								name: string;
-								description: string;
-								imageLink: string;
-							}) => (
-								<tr className="border " key={trader.name}>
-									<th className="border">
-										<img src={trader.imageLink} alt="trader image" />
-										{trader.name}
-									</th>
-									<th className="border p-4 text-left font-light">
-										{trader.description}
-									</th>
-								</tr>
-							)
-						)}
-					</tbody>
-				</table>
-			</div>
 			All items
 			{status === "success" ? (
 				<>
@@ -142,8 +149,46 @@ function TarkovList() {
 							<li key={item.id}>{item.name}</li>
 						))}
 					</ul>
-					<button onClick={() => prevPage()}>Prev</button>
-					<button onClick={() => nextPage()}>Next</button>
+					<div className="flex flex-row justify-between gap-5 mt-5 font-bold">
+						<a
+							className="group flex items-center cursor-pointer hover:text-slate-900 dark:hover:text-white"
+							onClick={() => prevPage()}
+						>
+							<svg
+								viewBox="0 0 3 6"
+								className="mr-3 w-auto h-1.5 text-slate-400 overflow-visible group-hover:text-slate-600 dark:group-hover:text-slate-300"
+							>
+								<path
+									d="M3 0L0 3L3 6"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path>
+							</svg>
+							Previous
+						</a>
+						<a
+							className="group flex items-center cursor-pointer hover:text-slate-900 dark:hover:text-white"
+							onClick={() => nextPage()}
+						>
+							Next
+							<svg
+								viewBox="0 0 3 6"
+								className="ml-3 w-auto h-1.5 text-slate-400 overflow-visible group-hover:text-slate-600 dark:group-hover:text-slate-300"
+							>
+								<path
+									d="M0 0L3 3L0 6"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path>
+							</svg>
+						</a>
+					</div>
 				</>
 			) : (
 				"loading"
