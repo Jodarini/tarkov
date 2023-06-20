06870b288f0faf5547925327d7f1e4a37c2e0be4
Jodarini <rivasjose9428@gmail.com>
2023-06-20 15:58:32 -0500

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { type AppType } from "next/dist/shared/lib/utils";
+import { QueryClientProvider, QueryClient} from "react-query";
+import "~/styles/globals.css";
+
+const queryClient = new QueryClient();
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
+};
+
+export default MyApp;
