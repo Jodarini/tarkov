9939ef6a03f3e45134091c4f752e076e2e568966
jodarini <rivasjose9428@gmail.com>
2023-07-21 12:49:33 -0500

    Traders loading state;

--- src/pages/traders.tsx
+++ src/pages/traders.tsx
@@ -45,16 +45,60 @@ export default function Traders() {
   return (
     <>
       <h3 className="mb-4 text-2xl font-bold">Traders</h3>
-      <table className="border-collapse border-slate-600">
+      <table className="border-collapse border-slate-600 text-left">
         <thead>
           <tr className="border-b border-slate-600 p-2">
-            <th colSpan={2} className="p-2">
+            <th colSpan={2} className="p-2 text-center">
               Name
             </th>
-            <th className="p-2">description</th>
+            <th className="p-2">Description</th>
           </tr>
         </thead>
         <tbody>
+          {isLoading && (
+            <>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+              <tr className="border-b border-slate-600">
+                <td className="p-2">loading...</td>
+                <td className="p-2"></td>
+                <td className="p-2"></td>
+              </tr>
+            </>
+          )}
           {data?.data.traders.map((trader) => (
             <tr className="border-b border-slate-600" key={trader.name}>
               <td className="p-2">
